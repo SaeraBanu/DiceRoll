af9f5938f9f5607a185a97f3ddb62bbca344a78a
SaeraBanu <75577817+SaeraBanu@users.noreply.github.com>
2021-01-06 07:41:23 +0530

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.round(Math.random()*5)+1;
+var imageLeft= "images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src",imageLeft);
+var randomNumber2 = Math.round(Math.random()*5)+1;
+var imageRight= "images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src",imageRight);
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="Player1 wins!";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent="Player2 wins!";
+}
+else{
+  document.querySelector("h1").textContent="Draw!";
+}
